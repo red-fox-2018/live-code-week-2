91bfe87c52277c2320e0f904c105c48f5aca3891
Iswanul Umam <umam@sepulsa.com>
2018-04-12 09:51:49 +0700

    finish soal 1

--- logic/index.js
+++ logic/index.js
@@ -1,22 +1,48 @@
 /**
+ * 
  * @author: Iswanul Umam - Red Fox
  */
 
-function tree(n){
-  var result = [];
-      result[0] = [1];
-      result[1] = [1,1];
-  for (var row = 2; row < n; row++){
-      result[row] = [1];
-      for (var col = 1; col <= row -1; col++){
-          result[row][col] = result[row-1][col] + result[row-1][col-1];
-          result[row].push(1);
-      }
+function tree(start, level, divider) {
+  let checkNumber = function (n, k, s) {
+    let res = s;
+    if (k > n - k) {
+      k = n - k;
+    }
+    for (let i = 0; i < k; ++i) {
+      res *= (n - i);
+      res /= (i + 1);
+    }
+    return res;
   }
-  return result;
+
+  let result = [];
+	for (let line = 0; line < level; line++) {
+    result[line] = [];
+		for (let i = 0; i <= line; i++) {
+      result[line].push(checkNumber(line, i, start));
+    }
+  }
+
+  let total = 0;
+  for (let res of result) {
+    for (let value of res) {
+      if (value % divider == 0) {
+        total++;
+      }
+    }
+  }
+  return total;
 }
-  
-for (var i = 0; i < tree(10).length; i++) {
-    // document.write(tree(10)[i]+"<br>");
-    console.log(tree(10)[i]);
-}
+
+// driver code -----------------------------------
+
+console.log(tree(1, 6, 3))  //output: 3
+console.log(tree(1, 6, 2))  //output: 6
+
+console.log(tree(1, 8, 3))  //output: 9
+console.log(tree(1, 8, 8))  //output: 0
+
+console.log(tree(5, 6, 5))  //output: 21
+console.log(tree(5, 6, 10)) //output: 6
+
